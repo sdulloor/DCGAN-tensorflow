b5fe5e42a7a2408aab1cde4b1ce50c7b972d33a0
Marcella Cindy Prasetio <m.cindy.prasetio@gmail.com>
2017-11-21 23:55:14 -0800

    Implemented DenseCGAN

--- model.py
+++ model.py
@@ -306,25 +306,14 @@ class DCGAN(object):
     with tf.variable_scope("discriminator") as scope:
       if reuse:
         scope.reuse_variables()
-      self.d_bn1 = batch_norm(name='d_bn1')
-      self.d_bn2 = batch_norm(name='d_bn2')
 
-      yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
-      x = conv_cond_concat(image, yb)
+      x = tf.reshape(image, [self.batch_size, -1])
+      xy = concat([x, y], 1)
 
-      h0 = lrelu(conv2d(x, self.c_dim + self.y_dim, name='d_h0_conv'))
-      h0 = conv_cond_concat(h0, yb)
+      h0 = lrelu(linear(xy, self.dfc_dim, 'd_h0_lin'))
+      h1 = linear(h0, 1, 'd_h1_lin')
 
-      h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim + self.y_dim, name='d_h1_conv')))
-      h1 = tf.reshape(h1, [self.batch_size, -1])
-      h1 = concat([h1, y], 1)
-
-      h2 = lrelu(self.d_bn2(linear(h1, self.dfc_dim, 'd_h2_lin')))
-      h2 = concat([h2, y], 1)
-
-      h3 = linear(h2, 1, 'd_h3_lin')
-
-      return tf.nn.sigmoid(h3), h3
+      return tf.nn.sigmoid(h1), h1
 
   def dcgan_generator(self, z, y):
     with tf.variable_scope("generator") as scope:
@@ -371,29 +360,13 @@ class DCGAN(object):
       self.g_bn2 = batch_norm(name='g_bn2')
 
       s_h, s_w = self.output_height, self.output_width
-      s_h2, s_h4 = int(s_h/2), int(s_h/4)
-      s_w2, s_w4 = int(s_w/2), int(s_w/4)
 
-      # yb = tf.expand_dims(tf.expand_dims(y, 1),2)
-      yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
-      z = concat([z, y], 1)
+      zy = concat([z, y], 1)
+      h0 = tf.nn.relu(linear(zy, self.gfc_dim, 'g_h0_lin'))
+      h1 = linear(h0, s_h * s_w * self.c_dim, 'g_h1_lin')
+      h1 = tf.reshape(h1, [self.batch_size, s_h, s_w, self.c_dim])
 
-      h0 = tf.nn.relu(
-          self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin')))
-      h0 = concat([h0, y], 1)
-
-      h1 = tf.nn.relu(self.g_bn1(
-          linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin')))
-      h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
-
-      h1 = conv_cond_concat(h1, yb)
-
-      h2 = tf.nn.relu(self.g_bn2(deconv2d(h1,
-          [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2')))
-      h2 = conv_cond_concat(h2, yb)
-
-      return tf.nn.sigmoid(
-          deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
+      return tf.nn.sigmoid(h1)
 
   def dcgan_sampler(self, z, y):
     with tf.variable_scope("generator") as scope:
@@ -438,26 +411,13 @@ class DCGAN(object):
       self.g_bn2 = batch_norm(name='g_bn2')
 
       s_h, s_w = self.output_height, self.output_width
-      s_h2, s_h4 = int(s_h/2), int(s_h/4)
-      s_w2, s_w4 = int(s_w/2), int(s_w/4)
 
-      # yb = tf.reshape(y, [-1, 1, 1, self.y_dim])
-      yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
-      z = concat([z, y], 1)
+      zy = concat([z, y], 1)
+      h0 = tf.nn.relu(linear(zy, self.gfc_dim, 'g_h0_lin'))
+      h1 = linear(h0, s_h * s_w * self.c_dim, 'g_h1_lin')
+      h1 = tf.reshape(h1, [self.batch_size, s_h, s_w, self.c_dim])
 
-      h0 = tf.nn.relu(self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin'), train=False))
-      h0 = concat([h0, y], 1)
-
-      h1 = tf.nn.relu(self.g_bn1(
-          linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin'), train=False))
-      h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
-      h1 = conv_cond_concat(h1, yb)
-
-      h2 = tf.nn.relu(self.g_bn2(
-          deconv2d(h1, [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2'), train=False))
-      h2 = conv_cond_concat(h2, yb)
-
-      return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
+      return tf.nn.sigmoid(h1)
 
   def discriminator(self, image, y=None, reuse=False):
     #return self.dcgan_discriminator(image, y, reuse)
