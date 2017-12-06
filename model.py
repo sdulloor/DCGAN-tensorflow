b6fecd294e586d6cd3d8cb6be5551e12611d4b11
Dulloor <dulloor@gmail.com>
2017-12-06 09:32:44 -0800

    Remove duplicate code. Merge network def for generator and sampler.

--- model.py
+++ model.py
@@ -318,31 +318,6 @@ class DCGAN(object):
 
       return tf.nn.sigmoid(h4), h4
 
-  def dcgan_cond_discriminator(self, image, y, reuse):
-    with tf.variable_scope("discriminator") as scope:
-      if reuse:
-        scope.reuse_variables()
-      # batch normalization : deals with poor initialization helps gradient flow
-      self.d_bn1 = batch_norm(name='d_bn1')
-      self.d_bn2 = batch_norm(name='d_bn2')
-
-      yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
-      x = conv_cond_concat(image, yb)
-
-      h0 = lrelu(conv2d(x, self.c_dim + self.y_dim, name='d_h0_conv'))
-      h0 = conv_cond_concat(h0, yb)
-
-      h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim + self.y_dim, name='d_h1_conv')))
-      h1 = tf.reshape(h1, [self.batch_size, -1])
-      h1 = concat([h1, y], 1)
-
-      h2 = lrelu(self.d_bn2(linear(h1, self.dfc_dim, 'd_h2_lin')))
-      h2 = concat([h2, y], 1)
-
-      h3 = linear(h2, 1, 'd_h3_lin')
-
-      return tf.nn.sigmoid(h3), h3
-
   def dcgan_generator(self, z, y):
     with tf.variable_scope("generator") as scope:
       self.g_bn0 = batch_norm(name='g_bn0')
@@ -381,37 +356,6 @@ class DCGAN(object):
 
       return tf.nn.tanh(h4)
 
-  def dcgan_cond_generator(self, z, y):
-    with tf.variable_scope("generator") as scope:
-      self.g_bn0 = batch_norm(name='g_bn0')
-      self.g_bn1 = batch_norm(name='g_bn1')
-      self.g_bn2 = batch_norm(name='g_bn2')
-
-      s_h, s_w = self.output_height, self.output_width
-      s_h2, s_h4 = int(s_h/2), int(s_h/4)
-      s_w2, s_w4 = int(s_w/2), int(s_w/4)
-
-      # yb = tf.expand_dims(tf.expand_dims(y, 1),2)
-      yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
-      z = concat([z, y], 1)
-
-      h0 = tf.nn.relu(
-          self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin')))
-      h0 = concat([h0, y], 1)
-
-      h1 = tf.nn.relu(self.g_bn1(
-          linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin')))
-      h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
-
-      h1 = conv_cond_concat(h1, yb)
-
-      h2 = tf.nn.relu(self.g_bn2(deconv2d(h1,
-          [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2')))
-      h2 = conv_cond_concat(h2, yb)
-
-      return tf.nn.sigmoid(
-          deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
-
   def dcgan_sampler(self, z, y):
     with tf.variable_scope("generator") as scope:
       scope.reuse_variables()
@@ -446,9 +390,36 @@ class DCGAN(object):
 
       return tf.nn.tanh(h4)
 
-  def dcgan_cond_sampler(self, z, y):
+  ## cDCGAN  networks
+  def dcgan_cond_discriminator(self, image, y, reuse):
+    with tf.variable_scope("discriminator") as scope:
+      if reuse:
+        scope.reuse_variables()
+      # batch normalization : deals with poor initialization helps gradient flow
+      self.d_bn1 = batch_norm(name='d_bn1')
+      self.d_bn2 = batch_norm(name='d_bn2')
+
+      yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
+      x = conv_cond_concat(image, yb)
+
+      h0 = lrelu(conv2d(x, self.c_dim + self.y_dim, name='d_h0_conv'))
+      h0 = conv_cond_concat(h0, yb)
+
+      h1 = lrelu(self.d_bn1(conv2d(h0, self.df_dim + self.y_dim, name='d_h1_conv')))
+      h1 = tf.reshape(h1, [self.batch_size, -1])
+      h1 = concat([h1, y], 1)
+
+      h2 = lrelu(self.d_bn2(linear(h1, self.dfc_dim, 'd_h2_lin')))
+      h2 = concat([h2, y], 1)
+
+      h3 = linear(h2, 1, 'd_h3_lin')
+
+      return tf.nn.sigmoid(h3), h3
+
+  def _dcgan_cond_generator(self, z, y, train=True, reuse=False):
     with tf.variable_scope("generator") as scope:
-      scope.reuse_variables()
+      if reuse:
+        scope.reuse_variables()
 
       self.g_bn0 = batch_norm(name='g_bn0')
       self.g_bn1 = batch_norm(name='g_bn1')
@@ -462,20 +433,27 @@ class DCGAN(object):
       yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
       z = concat([z, y], 1)
 
-      h0 = tf.nn.relu(self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin'), train=False))
+      h0 = tf.nn.relu(self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin'), train=train))
       h0 = concat([h0, y], 1)
 
       h1 = tf.nn.relu(self.g_bn1(
-          linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin'), train=False))
+          linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin'), train=train))
       h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
       h1 = conv_cond_concat(h1, yb)
 
       h2 = tf.nn.relu(self.g_bn2(
-          deconv2d(h1, [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2'), train=False))
+          deconv2d(h1, [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2'), train=train))
       h2 = conv_cond_concat(h2, yb)
 
-      return tf.nn.sigmoid(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
+      return tf.nn.tanh(deconv2d(h2, [self.batch_size, s_h, s_w, self.c_dim], name='g_h3'))
 
+  def dcgan_cond_generator(self, z, y):
+    return self._dcgan_cond_generator(z, y, train=True, reuse=False)
+
+  def dcgan_cond_sampler(self, z, y):
+      return self._dcgan_cond_generator(z, y, train=False, reuse=True)
+
+  ## Discriminator and Generator networks
   def discriminator(self, image, y=None, reuse=False):
     if self.conditional:
       return self.dcgan_cond_discriminator(image, y, reuse)
